893c44aa30ad5d652ecdc2f310c30211eb35fa64
Noa <31667392+noacubestudio@users.noreply.github.com>
2022-11-22 22:14:05 +0100

    nav states and painting square grid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import { base } from "$app/paths";
+    import { page } from '$app/stores';
     import logo from '$lib/assets/logo.svg';
     import '$lib/styles/normalize.css';
     import '$lib/styles/global.css';
@@ -8,9 +9,9 @@
 <nav>
     <img alt="Personal logo wordmark spelling noa" src={logo} />
 
-    <a href="{base}/">Index</a>
-    <a href="{base}/art">Art</a>
-    <a href="{base}/music">Music</a>
+    <a href="{base}/" class:active={$page.url.pathname === "/"}>Index</a>
+    <a href="{base}/art" class:active={$page.url.pathname === "/art"}>Art</a>
+    <a href="{base}/music" class:active={$page.url.pathname === "/music"}>Music</a>
 </nav>
 
 <slot></slot>
@@ -20,10 +21,18 @@
         display: flex;
         align-items: center;
         gap: 2em;
+        margin-bottom: 3em;
     }
     a {
         font-weight: 600;
         text-decoration: none;
         color: black;
+        font-size: 1.1em;
+    }
+    .active {
+        color: gray;
+    }
+    a:hover {
+        text-decoration: underline;
     }
 </style>
--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,7 +1,48 @@
 <script>
-    import pTea from '$lib/assets/hpTea.png';
+    import pFrog1 from '$lib/assets/hpFrog1.png'
+    import pFrog2 from '$lib/assets/hpFrog2.png'
+    import pFrog3 from '$lib/assets/hpFrog3.png'
+    import pGizmo from '$lib/assets/hpGizmo.png'
+    import pTurtle from '$lib/assets/hpTurtle.png'
+    import pFog from '$lib/assets/hpFog.png'
+    import pTea from '$lib/assets/hpTea.png'
 </script>
 
-<h1>Art</h1>
-<p>work in progress</p>
-<img alt="Teacan Painting" src={pTea} />
+<div class="paintingGrid">
+    <div><img alt="Painting of a frog" src={pFrog1}/></div>
+    <div><img alt="Painting of a frog" src={pFrog2}/></div>
+    <div><img alt="Painting of a frog" src={pFrog3}/></div>
+    <div><img alt="Painting of my cat" src={pGizmo}/></div>
+    <div><img alt="Painting of a turtle" src={pTurtle}/></div>
+    <div><img alt="Painting of a cow" src={pFog}/></div>
+    <div><img alt="Painting of a tea can" src={pTea}/></div>
+</div>
+
+<style>
+    .paintingGrid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 0px;
+    }
+    .paintingGrid > div {
+        outline: solid #999 1px;
+        outline-offset: -0.5px;
+        aspect-ratio : 1 / 1;
+        padding: 1vw;
+        display: grid;
+    }
+    .paintingGrid > div::before {
+        content: "";
+        padding-bottom: 100%;
+        display: block;
+    }
+    .paintingGrid > div::before,
+    .paintingGrid > div > img {
+        grid-area: 1 / 1 / 2 / 2;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+</style>
