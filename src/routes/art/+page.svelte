596402b10e5864994bd063e9005f2e0dbd27b234
Noa <31667392+noacubestudio@users.noreply.github.com>
2022-11-25 01:55:59 +0100

    image sizes

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,23 +1,6 @@
 <script>
-    import pFrog1 from '$lib/assets/hpFrog1.png?webp'
-    import pFrog2 from '$lib/assets/hpFrog2.png?webp'
-    import pFrog3 from '$lib/assets/hpFrog3.png?webp'
-    import pGizmo from '$lib/assets/hpGizmo.png?webp'
-    import pTurtle from '$lib/assets/hpTurtle.png?webp'
-    import pHorse from '$lib/assets/hpHorse.png?webp'
-    import pFog from '$lib/assets/hpFog.png?webp'
-    import pTea from '$lib/assets/hpTea.png?webp'
-    import pFlowers from '$lib/assets/hpFlowers.png?webp'
-    import pAlgae from '$lib/assets/hpAlgae.png?webp'
-    import pAmsterdam from '$lib/assets/hpAmsterdam.png?webp'
-    import pPark from '$lib/assets/hpPark.png?webp'
-    import pSnow from '$lib/assets/hpSnow.png?webp'
-    import pHand from '$lib/assets/hpHand.png?webp'
-    import pLantern from '$lib/assets/hpLantern.png?webp'
-    import pMaps from '$lib/assets/hpMaps1.png?webp'
-    import pRainbow from '$lib/assets/hpRainbow.png?webp'
-    import pSunset from '$lib/assets/hpSunset.png?webp'
-    import pForest from '$lib/assets/hpForest.png?webp'
+    import Image from "svelte-image";
+    const threshold = 0.2;
 </script>
 
 <p class="introText">
@@ -25,97 +8,86 @@
     I use HeavyPaint on an iPad to paint, and reference online images or take my own.
 </p>
 
-<div class="paintingGrid">
-    <div class="format s"><img alt="Painting of a frog" src={pFrog2}/></div>
-    <div class="format v"><img alt="Painting of a frog" src={pFrog1}/></div>
-    <div class="format s"><img alt="Painting of a frog" src={pFrog3}/></div>
-    <div class="format h"><img alt="Painting of a turtle" src={pTurtle}/></div>
-    <div class="format h"><img alt="Painting of a horse" src={pHorse}/></div>
-    <div class="format v"><img alt="Painting of a cow" src={pFog}/></div>
-    <div class="format s"><img alt="Painting of my cat" src={pGizmo}/></div>
-
-    <div class="format v"><img alt="Painting of a tea can" src={pTea}/></div>
-    <div class="format h"><img alt="Painting of flowers in a vase on a table" src={pFlowers}/></div>
-
-    <div class="format v"><img alt="Painting of a small green lake in a forest" src={pAlgae}/></div>
-    <div class="format h"><img alt="Painting of a landscape from a random Google Streetview" src={pMaps}/></div>
-    <div class="format h"><img alt="Painting of a park" src={pPark}/></div>
-    <div class="format v"><img alt="Painting of a forest view from above" src={pForest}/></div>
-    <div class="format v"><img alt="Painting of a road surrounded by tall snow" src={pSnow}/></div>
-    <div class="format v"><img alt="Painting of a building in Amsterdam" src={pAmsterdam}/></div>
-    <div class="format s"><img alt="Painting of a rainbow above a street" src={pRainbow}/></div>
-    <div class="format s"><img alt="Painting of my hand" src={pHand}/></div>
-
-    <div class="format s"><img alt="Painting of a lantern in Italy" src={pLantern}/></div>
-    <div class="format v"><img alt="Painting of a sunset above a street" src={pSunset}/></div>
+<div class="paintings">
+    <div class="format s"><Image {threshold} wrapperClass="IMG s" src="images/hpFrog2.png" alt="Painting of a frog"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpFrog1.png" alt="Painting of a frog"/></div>
+    <div class="format s"><Image {threshold} wrapperClass="IMG s" src="images/hpFrog3.png" alt="Painting of a frog"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpTurtle.png" alt="Painting of a turtle"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpHorse.png" alt="Painting of a horse"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpFog.png" alt="Painting of a cow"/></div>
+    <div class="format s"><Image {threshold} wrapperClass="IMG s" src="images/hpGizmo.png" alt="Painting of a cat"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpTea.png" alt="Painting of a tea can"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpFlowers.png" alt="Painting of a vase with flowers"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpAlgae.png" alt="Painting of a lake in a forest"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpMaps1.png" alt="Painting of a Google Streetview screenshot"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpPark.png" alt="Painting of a park"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpForest.png" alt="Painting of a forest view from above"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpSnow.png" alt="Painting of a road surrounded by tall snow"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpAmsterdam.png" alt="Painting of a building in Amsterdam"/></div>
+    <div class="format s"><Image {threshold} wrapperClass="IMG s" src="images/hpRainbow.png" alt="Painting of a rainbow above a street"/></div>
+    <div class="format s"><Image {threshold} wrapperClass="IMG s" src="images/hpHand.png" alt="Painting of my hand"/></div>
+    <div class="format s"><Image {threshold} wrapperClass="IMG s" src="images/hpLantern.png" alt="Painting of a lantern in Italy"/></div>
+    <div class="format v"><Image {threshold} wrapperClass="IMG v" src="images/hpSunset.png" alt="Painting of a sunset above a street"/></div>
 </div>
 
+
 <style>
+    :root {
+        --grid-size: 540px;
+        --format-square-padding: 70px;
+        --format-other-padding: 20px;
+    }
     .introText {
         color: #AAA;
         padding-bottom: 3em;
         max-width: 40em;
         line-height: 160%;
     }
-    .paintingGrid {
+    .paintings {
         display: grid;
-        grid-template-columns: repeat(1, 1fr);
-        grid-gap: 0px;
+        grid-template-columns: repeat(1, var(--grid-size));
+    }
+    @media only screen and (min-width: 1200px) {
+        .paintings {
+            grid-template-columns: repeat(2, var(--grid-size));
+        }
+    }
+    @media only screen and (min-width: 1700px) {
+        .paintings {
+            grid-template-columns: repeat(3, var(--grid-size));
+        }
+    }
+    @media only screen and (min-width: 2300px) {
+        .paintings {
+            grid-template-columns: repeat(4, var(--grid-size));
+        }
+    }
+    .format {
+        padding-bottom: 3vw;
+    }
+    :global(.IMG *) {
+        width: 96vw;
     }
     @media only screen and (min-width: 600px) {
-        .paintingGrid {
-            grid-template-columns: repeat(2, 1fr);
-        }
-    }
-    @media only screen and (min-width: 900px) {
-        .paintingGrid {
-            grid-template-columns: repeat(3, 1fr);
-        }
-    }
-    @media only screen and (min-width: 1400px) {
-        .paintingGrid {
-            grid-template-columns: repeat(4, 1fr);
-        }
-    }
-    .paintingGrid > div {
-        outline: solid #333 1px;
+        .format {
+        padding: var(--format-other-padding);
+        background-color: transparent;
+        outline: 1px solid #333;
         outline-offset: -0.5px;
-        aspect-ratio : 1 / 1;
-        padding: 2vw;
-        display: grid;
-        position: relative;
-        transition: padding 0.2s;
+        }
+        .format.s {
+            padding: var(--format-square-padding);
+        }
+        :global(.IMG *) {
+            width: calc(var(--grid-size) - 2 * var(--format-other-padding));
+            height: calc(var(--grid-size) - 2 * var(--format-other-padding));
+            object-fit: contain;
+        }
+        :global(.IMG.s *) {
+            width: calc(var(--grid-size) - 2 * var(--format-square-padding));
+            height: calc(var(--grid-size) - 2 * var(--format-square-padding));
+            object-fit: contain;
+        }
     }
-    .paintingGrid > div.format.s {
-        padding: 3vw;
-    }
-    .paintingGrid > div:hover {
-        padding: 1vw;
-        background-color: #111;
-    }
-    .paintingGrid > div.format.s:hover {
-        padding: 2vw;
-        background-color: #111;
-    }
-    .paintingGrid > div::before {
-        content: "";
-        padding-bottom: 100%;
-        display: block;
-    }
-    .paintingGrid > div::before,
-    .paintingGrid > div > img {
-        grid-area: 1 / 1 / 2 / 2;
-    }
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: contain;
-    }
-    .paintingGrid > div > img {
-        position: absolute;
-        max-width: 100%;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
+    
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from "@sveltejs/adapter-static";
+import svelteImage from 'svelte-image';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
     paths: {
       base: process.env.NODE_ENV === "production" ? "" : "",
     },
   },
+  preprocess: [
+    svelteImage({
+      placeholder: "blur"
+    }),
+  ],
 };
 
 export default config;
